d48d54d6d9d6387a88eb8dab05bbc53dbcc25010
Lisa Chen <lisachen@udel.edu>
2017-11-09 10:48:51 -0500

    new files

--- songbase.py
+++ songbase.py
@@ -1,15 +1,33 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
 @app.route('/')
 def index ():
-    return '<h1>hello world!!!</h1>'
+    # return '<h1>hello world!!!</h1>'
+    return render_template('index.html')
+
+
+@app.route('/my-songs')
+def get_user(name):
+    #return 'hello %s, your age is %d' % (name, 3)
+    return render_template('user.html', user_name = name)
 
 
 @app.route('/user/<string:name>/')
 def get_user(name):
-    return 'hello %s %d' % (name, 3)
+    #return 'hello %s, your age is %d' % (name, 3)
+    return render_template('user.html', user_name = name)
+
+
+@app.route('/songs')
+def get_all_songs():
+    songs = [
+    'song 1',
+    'song 2',
+    'song 3'
+    ]
+    return render_template('songs.html', songs = songs)
 
 
 @app.route('/users')
